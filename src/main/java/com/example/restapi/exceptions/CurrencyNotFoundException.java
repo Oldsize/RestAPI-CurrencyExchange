f105a7af4b0b0f0edf8c62c55aa9e63485af69e4
springboot <gytkivgu200gyt@gmail.com>
2024-06-13 15:35:55 +0300

    rearrange: CurrencyNotFoundException - deleted Exception

--- src/main/java/com/example/restapi/exceptions/CurrencyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.restapi.exceptions;
-
-public class CurrencyNotFoundException extends Exception {
-    public CurrencyNotFoundException() {
-        super("Currency Not Found.");
-    }
-}
