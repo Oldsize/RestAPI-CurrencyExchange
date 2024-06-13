bffcae707bee404a2e49d341f38ed0d3fcda0bd7
springboot <gytkivgu200gyt@gmail.com>
2024-06-13 15:30:18 +0300

    feat: added method currencyAmountExchange

--- src/main/java/com/example/restapi/service/ExchangeService.java
+++ src/main/java/com/example/restapi/service/ExchangeService.java
@@ -2,12 +2,12 @@ package com.example.restapi.service;
 
 import com.example.restapi.dao.CurrencyExchangeDAO;
 import com.example.restapi.dto.ExchangeDTO;
-import com.example.restapi.exceptions.CurrencyNotFoundException;
+import com.example.restapi.dto.ExchangeRateDTO;
 import com.example.restapi.exceptions.DAOException;
 import com.example.restapi.exceptions.DatabaseNotFoundException;
 import com.example.restapi.model.ExchangeRate;
 import com.example.restapi.utils.PreparedRequestsSQL;
-
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
@@ -27,14 +27,36 @@ public class ExchangeService {
         return INSTANCE;
     }
 
+    public ExchangeRateDTO currencyAmountExchange(String baseCurrencyCode,
+                                                  String targetCurrencyCode,
+                                                  Long amount) throws DAOException, SQLException, DatabaseNotFoundException {
+        if (!baseCurrencyCode.matches("^[a-zA-Z]{3}$") || !targetCurrencyCode.matches("^[a-zA-Z]{3}$"))
+            return null;
+        else if (amount == null)
+            return null;
+        ExchangeRate exchangeRate = currencyExchangeDAO.getExchangeByCodeQuery(baseCurrencyCode, targetCurrencyCode);
+        if (exchangeRate == null)
+            return null;
+        BigDecimal amountBigDecimal = new BigDecimal(amount);
+        BigDecimal convertedAmount = amountBigDecimal.multiply(exchangeRate.getRate());
+        ExchangeRateDTO exchangeRateDTO = new ExchangeRateDTO(exchangeRate.getId(),
+                                                              exchangeRate.getBaseCurrency(),
+                                                              exchangeRate.getTargetCurrency(),
+                                                              exchangeRate.getRate(),
+                                                              amount,
+                                                              convertedAmount);
+        return exchangeRateDTO;
+    }
 
-
-    public ExchangeDTO currencyExchange(String baseCurrencyCode, String targetCurrencyCode) throws DAOException, DatabaseNotFoundException, CurrencyNotFoundException, SQLException {
+    public ExchangeDTO currencyExchange(String baseCurrencyCode, String targetCurrencyCode) throws DAOException, DatabaseNotFoundException, SQLException {
         ExchangeRate exchangeModel = currencyExchangeDAO.getExchangeByCodeQuery(baseCurrencyCode, targetCurrencyCode);
-        ExchangeDTO exchangeDTO = new ExchangeDTO (exchangeModel.getId(),
-                                                   exchangeModel.getBaseCurrency().getId(),
-                                                   exchangeModel.getTargetCurrency().getId(),
-                                                   exchangeModel.getRate());
+        if (exchangeModel == null)
+            return null;
+
+        ExchangeDTO exchangeDTO = new ExchangeDTO(exchangeModel.getId(),
+                    exchangeModel.getBaseCurrency().getId(),
+                    exchangeModel.getTargetCurrency().getId(),
+                    exchangeModel.getRate());
         return exchangeDTO;
     }
 }
