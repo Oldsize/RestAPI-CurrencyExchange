131143ac949fdef1fecfc3ebedbd65ea00f30fef
springboot <gytkivgu200gyt@gmail.com>
2024-06-13 15:38:33 +0300

    feat: added handling errors

--- src/main/java/com/example/restapi/servlets/AddCurrencyServlet.java
+++ src/main/java/com/example/restapi/servlets/AddCurrencyServlet.java
@@ -3,13 +3,13 @@ package com.example.restapi.servlets;
 import com.example.restapi.dao.CurrencyDAO;
 import com.example.restapi.dto.CurrencyDTO;
 import com.example.restapi.mapper.CurrencyMapper;
+import com.example.restapi.mapper.StringMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -19,23 +19,29 @@ public class AddCurrencyServlet extends HttpServlet {
     @SneakyThrows
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        CurrencyMapper currencyMapper = new CurrencyMapper();
         String parameterCode = req.getParameter("code");
         String parameterFullName = req.getParameter("fullname");
+        resp.setContentType("application/json");
         String parameterSign = req.getParameter("sign");
+        StringMapper stringMapper = new StringMapper();
         if (parameterSign == null || parameterCode == null || parameterFullName == null) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameters");
+            String message = "Недостаток параметров.";
+            out.println(stringMapper.mapFromStringtoJSON(message));
+            resp.setStatus(400);
             return;
         } else if (!parameterCode.matches("^(?!.*(.).*\\1)[A-Z]{3}$") ||
                 !parameterFullName.matches("^(?=[a-zA-Z\\s]{1,30}$)(?=(?:.*[A-Z]){1,3})([a-zA-Z\\s]+)$") ||
                 !parameterSign.matches("^[A-Z\\p{Punct}]$")) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid parameters format");
+            String message = "Переданы неподходящие параметры валюты.";
+            out.println(stringMapper.mapFromStringtoJSON(message));
+            resp.setStatus(400);
             return;
         } else {
-            PrintWriter out = resp.getWriter();
             CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
             CurrencyDTO currencyDTO = new CurrencyDTO(parameterCode, parameterFullName, parameterSign);
             currencyDAO.addCurrencyQuery(currencyDTO);
-            CurrencyMapper currencyMapper = new CurrencyMapper();
             out.print(currencyMapper.mapFromDTOtoJSON(currencyDTO));
             out.flush();
         }
