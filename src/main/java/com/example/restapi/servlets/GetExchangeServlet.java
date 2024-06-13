28b480543fff67e5703739d073e2d2fd59c4a057
springboot <gytkivgu200gyt@gmail.com>
2024-06-13 15:25:30 +0300

    feat: added handling errors

--- src/main/java/com/example/restapi/servlets/GetExchangeServlet.java
+++ src/main/java/com/example/restapi/servlets/GetExchangeServlet.java
@@ -3,6 +3,7 @@ package com.example.restapi.servlets;
 import com.example.restapi.dao.CurrencyDAO;
 import com.example.restapi.dto.ExchangeDTO;
 import com.example.restapi.mapper.ExchangeMapper;
+import com.example.restapi.mapper.StringMapper;
 import com.example.restapi.model.Currency;
 import com.example.restapi.model.ExchangeRate;
 import com.example.restapi.service.ExchangeService;
@@ -11,25 +12,34 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
 
 @WebServlet(name = "getExchange", value = "/exchange/*")
-public class GetCurrencyExchangeServlet extends HttpServlet {
+public class GetExchangeServlet extends HttpServlet {
     @SneakyThrows
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ExchangeMapper exchangeMapper = new ExchangeMapper();
         ExchangeService exchangeService = ExchangeService.getInstance();
-        String pathInfo = req.getPathInfo().substring(1);
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        StringMapper stringMapper = new StringMapper();
+        String pathInfo = req.getPathInfo().substring(1).toUpperCase();
         if (!pathInfo.matches("^[A-Z]{6}$")) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            String message = "Неправильные параметры поиска обмена валют.";
+            out.print(stringMapper.mapFromStringtoJSON(message));
+            resp.setStatus(404);
         }
         CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
         String baseCurrencyCode = pathInfo.substring(0, 3);
         String targetCurrencyCode = pathInfo.substring(3);
         ExchangeDTO exchangeDTO = exchangeService.currencyExchange(baseCurrencyCode, targetCurrencyCode);
-        ExchangeMapper exchangeMapper = new ExchangeMapper();
+        if (exchangeDTO == null) {
+            String message = "Обменный курс не найден.";
+            out.print(stringMapper.mapFromStringtoJSON(message));
+            resp.setStatus(404);
+        }
         Currency baseCurrency = currencyDAO.getCurrencyByCodeQuery(baseCurrencyCode);
         Currency targetCurrency = currencyDAO.getCurrencyByCodeQuery(targetCurrencyCode);
         ExchangeRate exchange = new ExchangeRate();
@@ -37,10 +47,13 @@ public class GetCurrencyExchangeServlet extends HttpServlet {
         exchange.setRate(exchangeDTO.getRate());
         exchange.setBaseCurrency(baseCurrency);
         exchange.setTargetCurrency(targetCurrency);
+        if (exchange.getId() == null || exchange.getRate() == null || exchange.getBaseCurrency() == null || exchange.getTargetCurrency() == null) {
+            String message = "Обменный курс не найден.";
+            out.print(stringMapper.mapFromStringtoJSON(message));
+            resp.setStatus(404);
+        }
         String json = exchangeMapper.mapFromModeltoJSON(exchange);
-        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        PrintWriter out = resp.getWriter();
         out.print(json);
         out.flush();
     }
